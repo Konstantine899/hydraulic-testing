795b9ec308186e82d05eccb11d237a742845ff7e
Атрощенко Константин Анатольевич <asu8@vts.vitebsk.energo.net>
2021-05-24 08:21:47 +0300

    application-development-001 Вывел список адресов на страницу

--- src/components/view/ListCard/Table/Tbody/Row/Row.js
+++ src/components/view/ListCard/Table/Tbody/Row/Row.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
+import './Row.scss';
+
 function Row(props) {
   let Row = props.Row;
 
-  console.log('Row', Row);
-  return (
-    <tr>
-      <td>{Row.address}</td>
-    </tr>
-  );
+  const Address = Row.map(function (address, index) {
+    return (
+      <tr key={`${address.address} + ${index}`}>
+        <td>{address.address}</td>
+      </tr>
+    );
+  });
+
+  console.log('Address', Address);
+
+  return <React.Fragment>{Address}</React.Fragment>;
 }
 
 export default Row;
