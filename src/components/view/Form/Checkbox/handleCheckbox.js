66d17695a14f24c54f58fc2fd349f037b0ca2998
Атрощенко Константин Анатольевич <asu8@vts.vitebsk.energo.net>
2021-06-08 15:26:25 +0300

    reat: после перезагрузки состояние checkbox сохраняется

--- src/components/view/Form/Checkbox/handleCheckbox.js
+++ src/components/view/Form/Checkbox/handleCheckbox.js
@@ -1,15 +1,24 @@
-import React, { forwardRef, useState } from 'react';
+import React, { forwardRef, useEffect, useState } from 'react';
 import { Checkbox } from '@material-ui/core';
 
 export const HandleCheckbox = forwardRef((props, ref) => {
-  const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useState(
+    JSON.parse(localStorage.getItem(`eventChecked ${props.id}`))
+  );
 
   // Слушаю событие клика
   const toggleCheckbox = (event) => {
     const eventChecked = event.target.checked;
     setChecked(eventChecked);
-    localStorage.setItem(`eventChecked ${props.id}`, eventChecked);
+    localStorage.setItem(
+      `eventChecked ${props.id}`,
+      JSON.stringify(eventChecked)
+    );
   };
+
+  useEffect(() => {
+    toggleCheckbox;
+  }, [checked]);
 
   return <Checkbox checked={checked} onChange={toggleCheckbox} />;
 });
