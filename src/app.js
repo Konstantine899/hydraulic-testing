b619fa91284d9ea9cae651b7766ddd06e53c226c
Атрощенко Константин Анатольевич <asu8@vts.vitebsk.energo.net>
2021-07-01 11:00:59 +0300

    delete local and remote branches

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ import Routes from './components/controller/routes/Routes.js';
 
 import { DataProvider } from './components/model/services/DataContext/DataContext.js';
 
-// Удалил не нужные ветки
-
 export default function app() {
   return (
     <DataProvider>
